a929337dae5287353aaa0fc8acc84e5f7818a4ee
Jakub <jakub.matwis@gmail.com>
2021-12-16 00:31:44 +0100

    First changes in styling + new task into taskArr pushing on event

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,25 +1,38 @@
-const tasksArr = [];
+const taskArr = [];
 
-function Task(name, date, description) {
-  this.name = name;
-  this.date = date;
-  this.description = description;
-  this.type = type;
+class Task {
+  constructor(name, date, description) {
+    this.name = name;
+    this.date = date;
+    this.description = description;
+  }
+}
+
+function createNewTask(name, date, description) {
+  taskArr.push(new Task(name, date, description));
 }
 
 document.querySelector("#datePicker").valueAsDatetimeLocal = new Date();
+
+// Main view
 const tasksList = document.querySelector(".tasks");
 const addTaskBtn = document.querySelector(".addTaskBtn");
 const taskEditor = document.querySelector(".taskEditor");
-const body = document.querySelector(".body");
 const cancleEditorBtn = document.querySelector(".cancleAddingTask");
+const userInputs = document.querySelectorAll("input, textarea");
+// Editor
+const addNewTaskBtn = document.querySelector(".addNewTaskBtn");
+const cancleAddingTaskBtn = document.querySelector(".cancleAddingTask");
 
 addTaskBtn.addEventListener("click", () => {
   taskEditor.style.left = "0";
-  body.style.overflowY = "hidden";
 });
 
 cancleEditorBtn.addEventListener("click", () => {
   taskEditor.style.left = "100%";
-  body.style.overflowY = "scroll";
+});
+
+addNewTaskBtn.addEventListener("click", function (e) {
+  createNewTask(userInputs[0].value, userInputs[1].value, userInputs[2].value);
+  console.log(taskArr);
 });
