600adc49ff6e362fa09cdb4b60dda749882c5f4d
Jakub <jakub.matwis@gmail.com>
2021-12-17 17:09:03 +0100

    Last tweaks

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,4 +1,4 @@
-const taskArr = [];
+let taskArr = [];
 
 class Task {
   constructor(name, date, description) {
@@ -8,8 +8,54 @@ class Task {
   }
 }
 
+function updateDisplay() {
+  tasksList.innerHTML = "";
+  taskArr.forEach((task) => {
+    tasksList.innerHTML += `
+      <section class="task">
+        <div class="task__title">
+          <h1>${task.name}</h1>
+          <button class="taskBtn deleteTaskBtn">x</button>
+        </div>
+        <p>${task.date}</p>
+        <p class="task__description">${task.description}</p>
+      </section>
+    `;
+    updateDeleteTaskBtn();
+    saveTasks();
+  });
+}
+
+function updateDeleteTaskBtn() {
+  deleteTaskBtns = document.querySelectorAll(".deleteTaskBtn");
+  deleteTaskBtns.forEach((button, buttonIndex) => {
+    button.addEventListener("click", () => {
+      taskArr = taskArr.filter((_, taskIndex) => {
+        return taskIndex != buttonIndex;
+      });
+      console.log(taskArr);
+      updateDisplay();
+    });
+  });
+}
+
+function openEditor() {
+  taskEditor.style.left = "0";
+}
+
+function closeEditor() {
+  taskEditor.style.left = "100%";
+}
+
+function clearAllTasks() {
+  taskArr = [];
+  tasksList.innerHTML = `<div class="noTasksAlert">No planned tasks<div>`;
+}
+
 function createNewTask(name, date, description) {
   taskArr.push(new Task(name, date, description));
+  updateDisplay();
+  closeEditor();
 }
 
 document.querySelector("#datePicker").valueAsDatetimeLocal = new Date();
@@ -17,22 +63,28 @@ document.querySelector("#datePicker").valueAsDatetimeLocal = new Date();
 // Main view
 const tasksList = document.querySelector(".tasks");
 const addTaskBtn = document.querySelector(".addTaskBtn");
+const clearAllBtn = document.querySelector(".clearAllTasksBtn");
+let deleteTaskBtns = document.querySelectorAll(".deleteTaskBtn");
+// Editor
 const taskEditor = document.querySelector(".taskEditor");
+const addNewTaskBtn = document.querySelector(".addNewTaskBtn");
 const cancleEditorBtn = document.querySelector(".cancleAddingTask");
 const userInputs = document.querySelectorAll("input, textarea");
-// Editor
-const addNewTaskBtn = document.querySelector(".addNewTaskBtn");
-const cancleAddingTaskBtn = document.querySelector(".cancleAddingTask");
+
+clearAllBtn.addEventListener("click", () => {
+  clearAllTasks();
+});
 
 addTaskBtn.addEventListener("click", () => {
-  taskEditor.style.left = "0";
+  openEditor();
 });
 
 cancleEditorBtn.addEventListener("click", () => {
-  taskEditor.style.left = "100%";
+  closeEditor();
 });
 
 addNewTaskBtn.addEventListener("click", function (e) {
   createNewTask(userInputs[0].value, userInputs[1].value, userInputs[2].value);
-  console.log(taskArr);
 });
+
+updateDeleteTaskBtn();
